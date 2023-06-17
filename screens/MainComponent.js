a5a01227c9db56a4b8d9c43f16e188ae151e1fcc
Salem@2007 <esrom050603@gmail.com>
2023-06-16 20:09:37 -0600

    week 3 workshop

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -21,7 +21,7 @@ import {
   import ContactScreen from "./ContactScreen";
   import ReservationScreen from "./ReservationScreen";
   import { Icon } from "react-native-elements";
-  import logo from "../assets/images/logo.png";
+  import { logo } from "../public/images/logo.png";
   import { useDispatch } from "react-redux";
   import { useEffect } from "react";
   import { fetchPartners } from "../features/partners/partnersSlice";
