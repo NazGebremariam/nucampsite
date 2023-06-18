21c499eec8676bf891779941ae9b79cfea9cd931
Salem@2007 <esrom050603@gmail.com>
2023-06-17 21:37:20 -0600

    workshop week 3

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -6,10 +6,10 @@ import * as Animatable from 'react-native-animatable';
 
 const RenderCampsite = (props) => {
     const { campsite } = props;
-
     const view = useRef();
-
     const isLeftSwipe = ({ dx }) => dx < -200;
+    const isRightSwipe = ({ dx }) => dx >200;
+                   
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
@@ -18,79 +18,83 @@ const RenderCampsite = (props) => {
                 .rubberBand(1000)
                 .then((endState) =>
                     console.log(endState.finished ? 'finished' : 'canceled')
+                   
+                   
                 );
-        },
-        onPanResponderEnd: (e, gestureState) => {
+},
+    onPanResponderEnd: (e, gestureState) => {
             console.log('pan responder end', gestureState);
-            if (isLeftSwipe(gestureState)) {
-                Alert.alert(
-                    'Add Favorite',
-                    'Are you sure you wish to add ' +
-                        campsite.name +
-                        ' to favorites?',
-                    [
-                        {
-                            text: 'Cancel',
-                            style: 'cancel',
-                            onPress: () => console.log('Cancel Pressed')
-                        },
-                        {
-                            text: 'OK',
-                            onPress: () =>
-                                props.isFavorite
-                                    ? console.log('Already set as a favorite')
-                                    : props.markFavorite()
-                        }
-                    ],
-                    { cancelable: false }
-                );
+if (isLeftSwipe(gestureState)) {
+    Alert.alert(
+        'Add Favorite',
+        'Are you sure you wish to add ' +
+        campsite.name +
+        ' to favorites?',
+        [
+            {
+                text: 'Cancel',
+                style: 'cancel',
+                onPress: () => console.log('Cancel Pressed')
+            },
+            {
+                text: 'OK',
+                onPress: () =>
+                    props.isFavorite
+                        ? console.log('Already set as a favorite')
+                        : props.markFavorite()
             }
+        ],
+        { cancelable: false }
+    );
+} else if(isRightSwipe(gestureState)) {
+    props.onShowModal()
+}
         }
     });
 
-    if (campsite) {
-        return (
-            <Animatable.View
-                animation='fadeInDownBig'
-                duration={2000}
-                delay={1000}
-                ref={view}
-                {...panResponder.panHandlers}
-            >
-                <Card containerStyle={styles.cardContainer}>
-                    <Card.Image source={{ uri: baseUrl + campsite.image }}>
-                        <View style={{ justifyContent: 'center', flex: 1 }}>
-                            <Text style={styles.cardText}>{campsite.name}</Text>
-                        </View>
-                    </Card.Image>
-                    <Text style={{ margin: 20 }}>{campsite.description}</Text>
-                    <View style={styles.cardRow}>
-                        <Icon
-                            name={props.isFavorite ? 'heart' : 'heart-o'}
-                            type='font-awesome'
-                            color='#f50'
-                            raised
-                            reverse
-                            onPress={() =>
-                                props.isFavorite
-                                    ? console.log('Already set as a favorite')
-                                    : props.markFavorite()
-                            }
-                        />
-                        <Icon
-                            name='pencil'
-                            type='font-awesome'
-                            color='#5637DD'
-                            raised
-                            reverse
-                            onPress={props.onShowModal}
-                        />
+if (campsite) {
+    return (
+        <Animatable.View
+            animation='fadeInDownBig'
+            duration={2000}
+            delay={1000}
+            ref={view}
+            {...panResponder.panHandlers}
+        >
+            <Card containerStyle={styles.cardContainer}>
+                <Card.Image source={{ uri: baseUrl + campsite.image }}>
+                    <View style={{ justifyContent: 'center', flex: 1 }}>
+                        <Text style={styles.cardText}>{campsite.name}</Text>
                     </View>
-                </Card>
-            </Animatable.View>
-        );
-    }
-    return <View />;
+                </Card.Image>
+                <Text style={{ margin: 20 }}>{campsite.description}</Text>
+                <View style={styles.cardRow}>
+                    <Icon
+                        name={props.isFavorite ? 'heart' : 'heart-o'}
+                        type='font-awesome'
+                        color='#f50'
+                        raised
+                        reverse
+                        onPress={() =>
+                            props.isFavorite
+                                ? console.log('Already set as a favorite')
+                                : props.markFavorite()
+                        }
+                    />
+                    <Icon
+                        name='pencil'
+                        type='font-awesome'
+                        color='#5637DD'
+                        raised
+                        reverse
+                        onPress={props.onShowModal}
+                    />
+                </View>
+            </Card>
+        </Animatable.View>
+    );
+}
+return <View />;
 };
 
 const styles = StyleSheet.create({
